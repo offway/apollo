0f6ee79b9edbdfe1ecfd75edea7c9bc7714040aa
weinan <weinan@offway-art.com>
2019-03-27 13:15:53 +0800

    修改晒图排序

--- src/main/java/cn/offway/apollo/service/impl/PhShowImageServiceImpl.java
+++ src/main/java/cn/offway/apollo/service/impl/PhShowImageServiceImpl.java
@@ -64,7 +64,7 @@ public class PhShowImageServiceImpl implements PhShowImageService {
 				
                 Predicate[] predicates = new Predicate[params.size()];
                 criteriaQuery.where(params.toArray(predicates));
-                criteriaQuery.orderBy(criteriaBuilder.desc(root.get("status")));
+                criteriaQuery.orderBy(criteriaBuilder.desc(root.get("status")),criteriaBuilder.desc(root.get("createTime")));
 				return null;
 			}
 		}, page);
