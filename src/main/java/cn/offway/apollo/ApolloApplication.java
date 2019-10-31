b6f320de18f442129b8f75aaf10df98d479f8eff
XiaZhengXin <xzx@xiazhengxin.name>
2019-10-31 12:05:28 +0800

    测试 多数据源 ，配置文件修改。。。。加注解。。。
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

--- src/main/java/cn/offway/apollo/ApolloApplication.java
+++ src/main/java/cn/offway/apollo/ApolloApplication.java
@@ -1,17 +1,18 @@
 package cn.offway.apollo;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 @EnableSwagger2
 public class ApolloApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApolloApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApolloApplication.class, args);
+    }
 
 }
 
